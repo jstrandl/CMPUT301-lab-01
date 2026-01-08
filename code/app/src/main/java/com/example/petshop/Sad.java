b0d0c7b8e0e4722879a7c589d01ab5455e203d00
Jared Strandlund <jstrandl@ualberta.ca>
2026-01-07 18:13:11 -0700

    Reorganized

---
+++ code/app/src/main/java/com/example/petshop/Sad.java
@@ -0,0 +1,21 @@
+package com.example.petshop;
+
+import androidx.annotation.NonNull;
+
+import java.util.Date;
+
+public class Sad extends Mood {
+    public Sad() {
+        super();
+    }
+
+    public Sad(Date date) {
+        super(date);
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "Sad :(";
+    }
+}
